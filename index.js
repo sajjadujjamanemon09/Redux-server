bdea73f277ae837c4b383ec7f5b1a797e767a6f6
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2024-02-26 20:01:34 +0600

    Production into use vercel

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://nimble-tanuki-a7c6e7.netlify.app",
+      "http://localhost:5173/",
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200
+  })
+);
 
-const uri = "mongodb+srv://redux:jxpPp5QCvBhvw1Lb@cluster0.hepooac.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri =
+  "mongodb+srv://redux:jxpPp5QCvBhvw1Lb@cluster0.hepooac.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,38 +30,38 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const db = await client.db('taskmaster');
-    const tasksCollection = db.collection('tasks');
+    const db = await client.db("taskmaster");
+    const tasksCollection = db.collection("tasks");
 
-    console.log('Successfully connected to MongoDB!');
+    console.log("Successfully connected to MongoDB!");
 
-    app.get('/', (req, res) => {
-      res.send('Redux Server');
+    app.get("/", (req, res) => {
+      res.send("Redux Server");
     });
 
-    app.get('/tasks', async (req, res) => {
+    app.get("/tasks", async (req, res) => {
       try {
         const tasks = await tasksCollection.find({}).toArray();
         res.json(tasks);
       } catch (err) {
-        console.error('Error fetching tasks:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error("Error fetching tasks:", err);
+        res.status(500).json({ error: "Internal Server Error" });
       }
     });
 
-    app.post('/tasks', async (req, res) => {
+    app.post("/tasks", async (req, res) => {
       const newTask = req.body;
 
       try {
         const result = await tasksCollection.insertOne(newTask);
         res.status(201).json(result);
       } catch (err) {
-        console.error('Error creating task:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error("Error creating task:", err);
+        res.status(500).json({ error: "Internal Server Error" });
       }
     });
 
-    app.delete('/tasks/:id', async (req, res) => {
+    app.delete("/tasks/:id", async (req, res) => {
       const taskId = req.params.id;
 
       try {
@@ -59,17 +69,17 @@ async function run() {
           _id: ObjectId(taskId),
         });
         if (result.deletedCount === 0) {
-          res.status(404).json({ error: 'Task not found' });
+          res.status(404).json({ error: "Task not found" });
         } else {
-          res.json({ message: 'Task deleted successfully' });
+          res.json({ message: "Task deleted successfully" });
         }
       } catch (err) {
-        console.error('Error deleting task:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error("Error deleting task:", err);
+        res.status(500).json({ error: "Internal Server Error" });
       }
     });
 
-    app.patch('/tasks/:id', async (req, res) => {
+    app.patch("/tasks/:id", async (req, res) => {
       const taskId = req.params.id;
       const updatedTaskData = req.body;
 
@@ -80,13 +90,13 @@ async function run() {
         );
 
         if (result.matchedCount === 0) {
-          res.status(404).json({ error: 'Task not found' });
+          res.status(404).json({ error: "Task not found" });
         } else {
-          res.json({ message: 'Task updated successfully' });
+          res.json({ message: "Task updated successfully" });
         }
       } catch (err) {
-        console.error('Error updating task:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error("Error updating task:", err);
+        res.status(500).json({ error: "Internal Server Error" });
       }
     });
   } finally {
